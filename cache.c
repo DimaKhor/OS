fc9f1b4477d25d17fd7799395569c0d85c659b16
dima.khoroshev <110458174+DimaKhor@users.noreply.github.com>
2023-12-21 12:21:09 +0700

    Update cache.c
    add mutex to get_data_from_cache

--- cache.c
+++ cache.c
@@ -4,15 +4,16 @@ pthread_mutex_t cache_mutex = PTHREAD_MUTEX_INITIALIZER;
 void initialize_cache(Cache* cache) { cache->count = 0; }
 
 MemStruct* get_data_from_cache(Cache* cache, const char* url) {
-  for (int i = 0; i < cache->count; ++i) {
+    pthread_mutex_lock(&cache_mutex);
+    for (int i = 0; i < cache->count; ++i) {
     if (strcmp(cache->cache[i].url, url) == 0) {
-      cache->cache[i].count++;
+        cache->cache[i].count++;
         sleep(3);
-      return cache->cache[i].data;
+        return cache->cache[i].data;
     }
-  }
-
-  return NULL;
+    }
+    pthread_mutex_unlock(&cache_mutex);
+    return NULL;
 }
 void destroy_cache(Cache* cache) {
   pthread_mutex_lock(&cache_mutex);
@@ -44,32 +45,35 @@ void add_to_cache(Cache* cache, char* url, MemStruct* data) {
       return;
     }
   }
-  if (cache->count < MAX_CACHE_SIZE) {
-    printf(ANSI_COLOR_YELLOW
-           "Add to cache url (count < max) : |%s| \n" ANSI_COLOR_RESET,
-           url);
-    strcpy(cache->cache[cache->count].url, url);
-    cache->cache[cache->count].data = data;
-    cache->cache[cache->count].count = 1;
-    cache->count++;
-  } else {
-    printf(ANSI_COLOR_YELLOW
-           "Add to cache url (delete) : |%s| \n" ANSI_COLOR_RESET,
-           url);
+    if (cache->count < MAX_CACHE_SIZE) {
+       printf(ANSI_COLOR_YELLOW
+              "Add to cache url (count < max) : |%s| \n" ANSI_COLOR_RESET,
+              url);
+       strcpy(cache->cache[cache->count].url, url);
+       cache->cache[cache->count].data = data;
+       cache->cache[cache->count].count = 1;
+       cache->count++;
+     } else {
+       printf(ANSI_COLOR_YELLOW
+              "Add to cache url (delete) : |%s| \n" ANSI_COLOR_RESET,
+              url);
+       
+       int minInd = 0;
+       int min = cache->cache[0].count;
+       
+       for (int i = 1; i < cache->count; ++i) {
+         if (cache->cache[i].count < min) {
+           min = cache->cache[i].count;
+           minInd = i;
+         }
+       }
+       
+       strcpy(cache->cache[minInd].url, url);
+       free(cache->cache[minInd].data);
+       cache->cache[minInd].data = data;
+       cache->cache[minInd].count = 1;
+     }
+     
+     pthread_mutex_unlock(&cache_mutex);
+   }
 
-    int minInd = 0;
-    int min = cache->cache[0].count;
-
-    for (int i = 1; i < cache->count; ++i) {
-      if (cache->cache[i].count < min) {
-        min = cache->cache[i].count;
-        minInd = i;
-      }
-    }
-    strcpy(cache->cache[minInd].url, url);
-    free(cache->cache[minInd].data);
-    cache->cache[minInd].data = data;
-    cache->cache[minInd].count = 1;
-  }
-  pthread_mutex_unlock(&cache_mutex);
-}
